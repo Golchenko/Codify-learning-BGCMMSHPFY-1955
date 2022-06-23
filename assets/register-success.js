29184aa15ef51bde6dbc48e7f0f3c33310f3338c
Golchenko <nooiizze@gmail.com>
2022-06-23 12:14:15 +0300

    changed the concept of task implementation

---
+++ assets/register-success.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+console.log("!!!!!!!!!!!!!!!!");
+  });
+
+
+//   (function() {
+//     const REDIRECT_PATH = '/pages/customer-register-success';
+
+//     const selector = '#create_customer, form[action$="/account"][method="post"]',
+//         $form = document.querySelectorAll(selector)[0];
+
+//     if ($form) {
+//       $redirect = document.createElement('input');
+//       $redirect.setAttribute('name', 'return_to');
+//       $redirect.setAttribute('type', 'hidden');
+//       $redirect.value = REDIRECT_PATH;
+//       $form.appendChild($redirect);
+//     }
+//   })();
